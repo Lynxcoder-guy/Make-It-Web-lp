076dc28078c3b91738cd5f760f263ee92cafebb5
Shafeeq-lynxcoder <Shalbia549@gmail.com>
2026-06-01 10:36:21 +0700

    A bit of edit on the service deal

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function changeColor(tombol) {
 function showLayanan(layanan_current) {
     if (layanan_current === "lite") {
         nama_paket.innerHTML = "Paket Lite Web";
-        harga_paket.innerHTML = "Mulai dari 150K - 200K IDR <br><span style='font-size: 14px; font-weight: normal; color: #ccc;'>(Harga tergantung dengan panjang Website)</span>";
+        harga_paket.innerHTML = "Mulai dari 150K - 200K IDR <br><span style='font-size: 14px; font-weight: normal; color: #ccc;'>(Biaya menyesuaikan dengan jumlah konten dan fitur)</span>";
         changeColor(lite_btn);
 
         semua_paket[0].innerHTML = "Solusi paling hemat untuk brosur digital standar bagi bisnis yang baru merintis.";
@@ -55,14 +55,15 @@ function showLayanan(layanan_current) {
         semua_paket[2].innerHTML = "Sangat pas jika Anda hanya ingin memajang foto portofolio tanpa membutuhkan efek animasi yang kompleks.";
         semua_paket[3].innerHTML = "Pilihan standar yang aman untuk sekadar menampilkan info dasar bisnis Anda";
 
-        semua_paket[4].innerHTML = "Informasi profil, jasa, dan kontak dalam satu alur praktis.";
-        semua_paket[5].innerHTML = "Website statis interaktif yang ramah bagi bisnis kecil.";
-        semua_paket[6].innerHTML = "Tombol WhatsApp atau email untuk respon pelanggan instan.";
-        semua_paket[7].innerHTML = "Optimasi performa kecepatan akses tinggi guna kenyamanan pelanggan";
+        // 4 Baris Kedua (Apa yang akan anda dapatkan?)
+        semua_paket[4].innerHTML = "Profil bisnis lengkap yang memuat informasi layanan, keunggulan, serta detail kontak.";
+        semua_paket[5].innerHTML = "Tampilan website yang tertata rapi sehingga nyaman diakses dari ponsel maupun komputer.";
+        semua_paket[6].innerHTML = "Tombol WhatsApp atau email terintegrasi untuk memudahkan pelanggan menghubungi Anda secara instan.";
+        semua_paket[7].innerHTML = "Fleksibilitas pengembangan untuk menambah halaman atau fitur kapan saja seiring pertumbuhan bisnis Anda.";
 
     } else if (layanan_current === "smart") {
         nama_paket.innerHTML = "Paket Smart Web";
-        harga_paket.innerHTML = "Mulai dari 300K - 450K IDR <br><span style='font-size: 14px; font-weight: normal; color: #ccc;'>(Harga tergantung dengan panjang Website)</span>";
+        harga_paket.innerHTML = "Mulai dari 350k IDR <br><span style='font-size: 14px; font-weight: normal; color: #ccc;'>(Biaya menyesuaikan dengan jumlah konten dan fitur)</span>";
         changeColor(smart_btn);
 
         semua_paket[0].innerHTML = "Website terasa hidup, modern, dan sangat interaktif.";
@@ -72,12 +73,12 @@ function showLayanan(layanan_current) {
 
         semua_paket[4].innerHTML = "Semua fitur Paket Lite page dengan satu halaman praktis & ramah HP.";
         semua_paket[5].innerHTML = "Sentuhan kodingan JavaScript murni untuk elemen interaktif di web.";
-        semua_paket[6].innerHTML = "Fitur dinamis (seperti navigasi smooth, filter menu, atau toggle konten).";
+        semua_paket[6].innerHTML = "Fitur dinamis (seperti navigasi smooth, search menu, atau toggle konten).";
         semua_paket[7].innerHTML = "Halaman website yang dinamis dan penyampaian informasi intraktif";
 
     } else if (layanan_current === "multi") {
         nama_paket.innerHTML = "Paket Multi-Page Web";
-        harga_paket.innerHTML = "Mulai dari 350K IDR <br><span style='font-size: 14px; font-weight: normal; color: #ccc;'>(Gratis 3 halaman pertama)</span>";
+        harga_paket.innerHTML = "Mulai dari 500K IDR <br><span style='font-size: 14px; font-weight: normal; color: #ccc;'>(Gratis 3 halaman pertama)</span>";
         changeColor(multi_btn);
 
         semua_paket[0].innerHTML = "Solusi tepat untuk memaparkan informasi bisnis secara mendalam.";
